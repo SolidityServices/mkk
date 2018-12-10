8f055597a7983281d394cebf95fe02261af5bfbc
Anon <peter.ujvari92@gmail.com>
2018-12-10 15:41:55 +0100

    infura rinkeby deployment working

--- truffle.js
+++ truffle.js
@@ -1,10 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infura = require('./infura.json');
+
+require('dotenv').config(); // Store environment-specific variable from '.env' to process.env
+
 module.exports = {
   networks: {
-    dev: {
+    ganache: {
       host: 'localhost',
       port: 8545,
-      network_id: '*', // Match any network id
+      network_id: '*',
       gas: 5000000,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(infura.mnemonic, `https://rinkeby.infura.io/v3/${infura.apiKey}`),
+      network_id: 4,
+      gas: 5000000,
+      gasPrice: 20000000000, //20 gwei
     },
   },
   solc: {
