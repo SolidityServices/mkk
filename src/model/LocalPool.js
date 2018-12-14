2152e15e0a944dd8e9f87138924de0c4d67f92bd
Daniel Peter BAN <ban.daniel.peter@gmail.com>
2018-12-14 14:01:55 +0100

    fix pool creation

--- src/model/LocalPool.js
+++ src/model/LocalPool.js
@@ -27,12 +27,15 @@ export default class LocalPool {
 
   constructor(address) {
     this.connectIco = window.connectIco;
-    this.saleAddress = address;
+    this.poolAddress = address;
     this.init();
   }
 
   init() {
-    const params = this.connectIco.pool.getPoolParams(this.poolAddress);
+    let params = this.initParams();
+    if (this.poolAddress) {
+      params = this.connectIco.pool.getPoolParams(this.poolAddress);
+    }
     this.saleParticipateFunctionSig = params.saleParticipateFunctionSig;
     this.saleWithdrawFunctionSig = params.saleWithdrawFunctionSig;
     this.saleAddress = params.saleAddress;
@@ -50,5 +53,23 @@ export default class LocalPool {
     this.providerFeeRate = params.providerFeeRate;
     this.creatorFeeRate = params.creatorFeeRate;
     this.whitelistPool = params.whitelistPool;
+  }
+
+  initParams() {
+    const params = {};
+
+    params.saleAddress = '0x0000000000000000000000000000000000000000';
+    params.tokenAddress = '0x0000000000000000000000000000000000000000';
+    params.creatorFeeRate = 0;
+    params.saleStartDate = new Date();
+    params.saleEndDate = new Date();
+    params.minContribution = 0;
+    params.maxContribution = 0;
+    params.minPoolGoal = 0;
+    params.maxPoolAllocation = 0;
+    params.withdrawTimelock = new Date();
+    params.whitelistPool = false;
+
+    return params;
   }
 }
