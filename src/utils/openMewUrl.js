67e343891799fcf08925da14a06f11c1647ad38c
Daniel Peter BAN <ban.daniel.peter@gmail.com>
2019-02-15 09:45:49 +0100

    open mew url in new tab

--- src/utils/openMewUrl.js
+++ src/utils/openMewUrl.js
@@ -1,5 +1,5 @@
 export default function (url) {
-  window.location.href = url;
-  // const tab = window.open(url, '_blank');
-  // tab.focus();
+  // window.location.href = url;
+  const tab = window.open(url, '_blank');
+  tab.focus();
 }
