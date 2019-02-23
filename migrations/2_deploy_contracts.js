bd479784cf1a7a74e57eadaffa5e4aa406e77e06
Anon <peter.ujvari92@gmail.com>
2019-02-23 19:59:18 +0100

    automations migration fixed, pool factory migration parametized

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+var web3 = require('web3')
 var PoolFactory = artifacts.require('./PoolFactory.sol')
 var Pool = artifacts.require('./Pool.sol')
 var SafeMath = artifacts.require('../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol')
@@ -8,9 +9,21 @@ var Automations = artifacts.require('./Automations.sol')
 
 module.exports = function (deployer) {
 
+  var flatFee = 0.001 //in ETH
+  var maxAllocationFeeRate = 10 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
+  var maxCreatorFeeRate = 150 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
+  var providerFeeRate = 50 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
+
+  var addressList = [];
+  var countryList = [];
 
   deployer.deploy(KYC).then(function(){
-    return deployer.deploy(PoolFactory, KYC.address, 0, 0, 0, 0)
+    return deployer.deploy(PoolFactory, KYC.address, web3.utils.toWei(flatFee.toString()), maxAllocationFeeRate, maxCreatorFeeRate, providerFeeRate)
+  }).then(function(){
+    if(addressList.length > 0) KYC.deployed().then(instance => {
+      console.log("adding kyc addresses: " + addressList)
+      return instance.addKYCAddresses(addressList, countryList);
+    })
   })
 
 }
