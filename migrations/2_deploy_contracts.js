fa8d81aaa513982736b7db2cb7199785d94033a2
Anon <peter.ujvari92@gmail.com>
2019-02-28 23:31:19 +0100

    deleting web3 from deployment 2 again

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-var web3 = require('web3')
+//var web3 = require('web3')
 var PoolFactory = artifacts.require('./PoolFactory.sol')
 var Pool = artifacts.require('./Pool.sol')
 var SafeMath = artifacts.require('../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol')
@@ -9,8 +9,8 @@ var Automations = artifacts.require('./Automations.sol')
 
 module.exports = function (deployer) {
 
-  var flatFee = 0.001 //in ETH
-  var maxAllocationFeeRate = 10 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
+  var flatFee = 1000000000000000 //in wei
+  var maxAllocationFeeRate = 1 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
   var maxCreatorFeeRate = 150 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
   var providerFeeRate = 50 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
 
@@ -18,7 +18,7 @@ module.exports = function (deployer) {
   var countryList = ['HUN', 'HUN', 'HUN', 'HUN'];
 
   deployer.deploy(KYC).then(function(){
-    return deployer.deploy(PoolFactory, KYC.address, web3.utils.toWei(flatFee.toString()), maxAllocationFeeRate, maxCreatorFeeRate, providerFeeRate)
+    return deployer.deploy(PoolFactory, KYC.address, flatFee, maxAllocationFeeRate, maxCreatorFeeRate, providerFeeRate)
   }).then(function(){
     if(addressList.length > 0) KYC.deployed().then(instance => {
       instance.addKYCAddresses(addressList, countryList).then(() => {
