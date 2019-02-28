e5c72e57ad0e35fadb58a8240929106abde77e39
Anon <peter.ujvari92@gmail.com>
2019-02-28 20:51:41 +0100

    deleting web3 from deployment file 2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-var web3 = require('web3')
+//var web3 = require('web3')
 var PoolFactory = artifacts.require('./PoolFactory.sol')
 var Pool = artifacts.require('./Pool.sol')
 var SafeMath = artifacts.require('../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol')
@@ -9,7 +9,7 @@ var Automations = artifacts.require('./Automations.sol')
 
 module.exports = function (deployer) {
 
-  var flatFee = 0.001 //in ETH
+  var flatFee = 1000000000000000 //in wei
   var maxAllocationFeeRate = 10 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
   var maxCreatorFeeRate = 150 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
   var providerFeeRate = 50 //in 1 one 1000th e.g. 1 => 0.1%, 50 => 5%, 1000 => 100%
