52bc17f047a39c121ed813237864fe5e6b73654c
Daniel Peter BAN <ban.daniel.peter@gmail.com>
2019-03-01 14:22:38 +0100

    WIP: fix sol migrations

--- migrations/_4_pool.js
+++ migrations/_4_pool.js
@@ -1,11 +1,9 @@
-/* eslint-disable */
-//for testing purposes only
-let Pool = artifacts.require('./Pool.sol')
-let KYC = artifacts.require('./KYC.sol')
+// for testing purposes only
+const Pool = artifacts.require('./Pool.sol');
+const KYC = artifacts.require('./KYC.sol');
+const Web3 = require('web3');
 
-
-let Web3= require('web3');
-let web3 = new Web3();
+const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 let accounts;
 
@@ -14,20 +12,18 @@ let accounts;
   console.log(accounts);
 
   const balance = await web3.eth.getBalance(accounts[0]);
-  console.log("balance", web3.utils.fromWei(balance, "ether"));
+  console.log('balance', web3.utils.fromWei(balance, 'ether'));
 })();
 
-
-
-module.exports = function (deployer) {
-  deployer.deploy(KYC).then(function() {
-    console.log("KYC contract deployed and its address: ",KYC.address);
-    return deployer.deploy(Pool,
-      [KYC.address, accounts[1], accounts[0], accounts[2], accounts[3]],
-      ["bla","bla","bla"],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      false,
-      [],[],[],{from:accounts[0]}).then(function(){
+module.exports = (deployer) => {
+  deployer.deploy(KYC)
+    .then(() => {
+      console.log('KYC contract deployed and its address: ', KYC.address);
+      return deployer.deploy(Pool,
+        [KYC.address, accounts[1], accounts[0], accounts[2], accounts[3]],
+        ['bla', 'bla', 'bla'],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        false,
+        [], [], [], { from: accounts[0] });
     });
-  });
 };
