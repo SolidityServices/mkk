9545d757efc366461b55eaea8a5222a613a6b656
Gergő Molnár <gergo.molnar.dev@gmail.com>
2019-03-29 16:51:11 +0100

    Pool fee percent rate hotfix.

--- src/model/connect-ico/PoolFactory.js
+++ src/model/connect-ico/PoolFactory.js
@@ -174,7 +174,7 @@ export default class PoolFactory {
           pool.poolDescription,
         ],
         [
-          parseFloat(pool.creatorFeeRate), // convert percentage to "per thousandth"
+          parseFloat(pool.creatorFeeRate) * 100, // convert percentage to "per thousandth"
           Math.floor(pool.saleStartDate / 1000), // convert to unix timestamp
           Math.floor(pool.saleEndDate / 1000), // convert to unix timestamp
           pool.minContribution * 1000000000000000000, // convert ether to wei
@@ -215,7 +215,7 @@ export default class PoolFactory {
         pool.poolDescription,
       ],
       [
-        parseFloat(pool.creatorFeeRate), // convert percentage to integer
+        parseFloat(pool.creatorFeeRate) * 100, // convert percentage to integer
         Math.floor(pool.saleStartDate / 1000), // convert to unix timestamp
         Math.floor(pool.saleEndDate / 1000), // convert to unix timestamp
         pool.minContribution * 1000000000000000000, // convert ether to wei
@@ -241,9 +241,9 @@ export default class PoolFactory {
     const result = (poolCreationEventLog) ? poolCreationEventLog.args.poolAddress : null;
 
     // const result = receipt.logs[0].args.poolAddress;
-    console.log(poolCreationEventLog);
-    console.log(receipt);
-    console.log(`pool address: ${result}`);
+    // console.log(poolCreationEventLog);
+    // console.log(receipt);
+    // console.log(`pool address: ${result}`);
 
     return result;
   }
