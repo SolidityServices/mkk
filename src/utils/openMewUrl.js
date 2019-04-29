93acd7af1e7a45bbd5cb70b695f3b8f5025376a5
Gergő Molnár <gergo.molnar.dev@gmail.com>
2019-04-29 07:27:50 +0200

    Mobile browser error fix.

--- src/utils/openMewUrl.js
+++ src/utils/openMewUrl.js
@@ -1,5 +1,16 @@
 export default function (url) {
+  // Create link in memory
+  const a = window.document.createElement('a');
+  a.target = '_blank';
+  a.href = url;
+
+  // Dispatch fake click
+  const e = window.document.createEvent('MouseEvents');
+  e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  a.dispatchEvent(e);
+
+  // old code
   // window.location.href = url;
-  const tab = window.open(url, '_blank');
-  tab.focus();
+  // const tab = window.open(url, '_blank');
+  // tab.focus();
 }
