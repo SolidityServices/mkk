adc26f8ccabe33877934bc3e44b035c1a8f5de69
Gergő Molnár <gergo.molnar.dev@gmail.com>
2019-05-07 08:07:23 +0200

    window location change if new tab unavailable

--- src/utils/openMewUrl.js
+++ src/utils/openMewUrl.js
@@ -4,10 +4,17 @@ export default function (url) {
   a.target = '_blank';
   a.href = url;
 
-  // Dispatch fake click
-  const e = window.document.createEvent('MouseEvents');
-  e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-  a.dispatchEvent(e);
+  const tab = window.open(url, '_blank');
+  if (tab) {
+    tab.focus();
+  } else {
+    window.location.href = url;
+  }
+
+  // // Dispatch fake click
+  // const e = window.document.createEvent('MouseEvents');
+  // e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  // a.dispatchEvent(e);
 
   // old code
   // window.location.href = url;
